e1499ffaf1d2e803c4a054ac3cc025074f9a5064
Said <saidziani.pro@gmail.com>
2017-11-28 08:47:04 +0100

    adding 'self.lang' attribute to the class, 1=>Fr / 2=>Ar .

--- search.py
+++ search.py
@@ -1,27 +1,33 @@
 from bs4 import BeautifulSoup
-import os
-from nltk.tokenize import TweetTokenizer
-
-#Tokenisation
-tweet = "Hi! i'm Farid i have 1520$ #mony :-) :) <3 !"
+import os, nltk
 
 class Search():
 
-
-    def __init__(self, keywords = False, corpus = False):
+    def __init__(self, keywords = False, corpus = False, lang = 1):
         self.keywords = set(keywords)
         self.corpus = corpus
+        self.lang = lang
 
     # get a tagged text: exemple [je suis libre] => [('je', P), ('suis', VB), ('libre', ADJ)]
     def getTagged(self, text):
         from nltk.tag import StanfordPOSTagger
-        jar = 'stanford-french-pos-tagger/stanford-postagger-3.8.0.jar'
-        model = 'stanford-french-pos-tagger/french.tagger'
-        pos_tagger = StanfordPOSTagger(model, jar, encoding='utf8' )
-        tokenizer = TweetTokenizer()
-        tokenizedText = tokenizer.tokenize(text.lower())
-        taggedText = pos_tagger.tag(tokenizedText)
+
+        if self.lang == 1:
+            jar = 'stanford-pos-tagger/stanford-postagger-3.8.0.jar'
+            model = 'stanford-pos-tagger/french.tagger'
+            pos_tagger = StanfordPOSTagger(model, jar, encoding='utf8' )
+            tokenizedText = nltk.word_tokenize(text.lower())
+            taggedText = pos_tagger.tag(tokenizedText)
+        else:
+            jar = 'stanford-pos-tagger/stanford-postagger-3.8.0.jar'
+            model = 'stanford-pos-tagger/arabic.tagger'
+            pos_tagger = StanfordPOSTagger(model, jar, encoding='utf8' )
+            tokenizedText = nltk.word_tokenize(text.lower())
+            taggedText = pos_tagger.tag(tokenizedText)
+
         return taggedText
+
+
 
     # transform my XML page to Beautifull soup object
     def getCorpusSoup(self):
@@ -29,11 +35,12 @@ class Search():
         content = file.read()
         return BeautifulSoup(content,"lxml")
 
+
     # just to split sentence to words
-    def text2list(self, text):
-        tokenizer = TweetTokenizer()
-        tokenizedText = tokenizer.tokenize(text.lower())
+    def text2list(self, text):        
+        tokenizedText = nltk.word_tokenize(text.lower())
         return tokenizedText
+
 
     # check if my recipe contain a denied ingredient (porc, liqueur...)
     def isDenied(self, content, denied):
@@ -45,27 +52,52 @@ class Search():
                 return False
         return True
 
+
     # just to build correctly my denied word list: french stop words + colors ...etc
     def getDeniedWords(self):
         from nltk.corpus import stopwords
-        colors = ['blanc', 'noir', 'rouge', 'jaune'] # à compléter
-        otherWord = ['tous', 'les', 'ingrédients', 'moitiés', 'trait', 'frais', 'ensemble', 'petits', 'grand', 'préparation', 'préparations']
         deniedList = []
-        deniedList.extend(stopwords.words('french')) 
+        if self.lang == 1:
+            colors =  ['blanc', 'noir', 'rouge', 'jaune'] # à compléter
+            otherWord = ['tous', 'les', 'ingrédients', 'moitiés', 'trait', 'frais', 'ensemble', 'petits', 'grand', 'préparation', 'préparations', 'poisson', 'légumes', 'fruits', 'épices', 'service', 'plat']
+            haram = ['vin', 'porc', 'liqueur', 'cochenille', 'mulet']#à compléter
+            stopWords = stopwords.words('french')
+            stemmer = nltk.stem.snowball.FrenchStemmer()
+        else:
+            colors =  [] # à compléter
+            otherWord = []
+            haram = []#à compléter
+            from stop_words import get_stop_words
+            stopWords = get_stop_words('arabic')
+            from nltk.stem.isri import ISRIStemmer
+            stemmer = ISRIStemmer()
+
         deniedList.extend(colors)
         deniedList.extend(otherWord)
+        deniedList.extend(haram)
+        deniedList.extend(stopWords)
+        deniedList = [stemmer.stem(word) for word in deniedList]
         return deniedList
+
 
     # transform a recipe on XML format to a keywords list
     def getKeyWords(self, text):
-        deniedList = self.getDeniedWords() 
+        if self.lang == 1:
+            stemmer = nltk.stem.snowball.FrenchStemmer()
+        else:
+            from nltk.stem.isri import ISRIStemmer
+            stemmer = ISRIStemmer()
+        deniedList = self.getDeniedWords()
         keywords = []
         for line in text:
-            line = self.getTagged(line)
-            line = [taggedWord[0] for taggedWord in line if taggedWord[1].startswith('N')]
-            cleanText = [word for word in line if word not in deniedList and word.isalpha()]
+            tline = self.getTagged(line)
+            if self.lang != 1 :
+                tline = [(item[1].split('/')[0], item[1].split('/')[1]) for item in tline]
+            ntline = [taggedWord[0] for taggedWord in tline if taggedWord[1].startswith('N')]
+            cleanText = [stemmer.stem(word) for word in ntline if stemmer.stem(word) not in deniedList and word.isalpha()]
             keywords.extend(cleanText)
         return set(keywords)
+
 
     # search recipe by diffrent mode
     def getRecipe(self, mode):
@@ -90,17 +122,43 @@ class Search():
                 output.append(d.text)
         return output
 
+
     # search a recipe by ID
     def idSearch(self, idRecipe):
         soup = self.getCorpusSoup()
         recipe = [ balise.text for balise in soup.find_all("rec", attrs={"id" : idRecipe}) ]
         return recipe
 
+    # search a recipe by ID specific data
+    def idSearchData(self, idRecipe, mode):
+        #mode=[1 => title, 2 => category, 3 => ingredient]
+        soup = self.getCorpusSoup()
+        if mode == 1:
+            balise = "title"
+        elif mode == 2:
+            balise = "cat"
+        elif mode == 3:
+            balise = "ing"
+        elif mode == 4:
+            balise = "prep"
+        elif mode == 5:
+            balise = "eng"
+        else:
+            balise = "inf"
+
+        data = []
+
+        for div in soup.find_all("rec", attrs={"id" : str(idRecipe)}):
+            for d in div.find_all(balise):
+                data.append(d.text)
+        return data
+
     # get recipe ID
     def findById(self, rec):
         soup = BeautifulSoup(rec)
         idRecipe = soup.find("rec")['id']
         return idRecipe
+
 
     # check if a recipe is healthy (callories <= 300)
     def isHealthy(self, idRecipe):
@@ -119,7 +177,7 @@ class Search():
             print(keywords)
 
 
-    def existNb(self,keywords, recipeKeyWords):
+    def existNb(self, keywords, recipeKeyWords):
         cpt = 0
         for keyword in keywords:
             if keyword.lower() in recipeKeyWords:
@@ -127,27 +185,111 @@ class Search():
         return cpt, len(recipeKeyWords)
 
 
-    def compare(self,keywords, recipeKeyWords):
+    def compare(self, keywords, recipeKeyWords):
+
         nbWords, lenRecipeKeyWords = self.existNb(keywords, recipeKeyWords)
-        prop = (nbWords / lenRecipeKeyWords) * 100
-        print(prop)
+        prop = round(float((nbWords / lenRecipeKeyWords) * 100), 2)
+        # print(prop)
         if prop == 0:
-            return 0 
+            return -2, prop 
         elif prop < 30:
-            return -1
+            return -1, prop
         elif prop >= 30 and prop < 60: 
-            return 0
+            return 0, prop
         else:
-            return 1
+            return 1, prop
+
+
+    # non desirable ingredient
+    def getNonDesirable(self, choice):
+        if self.lang == 1:
+            notBio = ['acide'] #à compélter
+            notVegan = ['viandes', 'poisson', 'poulet'] #à compélter
+            diab = ['sucres', 'miel'] #à compélter
+            noCrustace = [''] #à compélter
+            noGlutin = [''] #à compélter
+            noArachides = [''] #à compélter
+            noLait = [''] #à compélter
+            stemmer = nltk.stem.snowball.FrenchStemmer()
+        else:
+            notBio = ['acide'] #à compélter
+            notVegan = ['viandes', 'poisson', 'poulet'] #à compélter
+            diab = ['sucres', 'miel'] #à compélter
+            noCrustace = [''] #à compélter
+            noGlutin = [''] #à compélter
+            noArachides = [''] #à compélter
+            noLait = [''] #à compélter
+            from nltk.stem.isri import ISRIStemmer
+            stemmer = ISRIStemmer()
+
+        nonDesirable = []
+        if 1 in choice: 
+            nonDesirable.extend(notBio)
+        if 2 in choice:
+            nonDesirable.extend(notVegan)
+        if 3 in choice:
+            nonDesirable.extend(diab)
+        if 4 in choice:
+            nonDesirable.extend(noCrustace)
+        if 5 in choice:
+            nonDesirable.extend(noGlutin)
+        if 6 in choice:
+            nonDesirable.extend(noArachides)
+        if 7 in choice:
+            nonDesirable.extend(noLait)
+        
+        nonDesirable = [stemmer.stem(word) for word in nonDesirable]
+        return nonDesirable
+
+
+    #lang=1 french / lang=2 arabe
+    def getResult(self, queryKeywords, lang, checked):
+
+        import re
+        from operator import itemgetter
+        lang = self.lang
+        corpusKeyWords = open('corpus/frKeyWords.txt', 'r') if lang == 1 else  open('corpus/arKeyWords.txt', 'r')
+        idRecipes = {}
+        recipesTocheck = {}
+        for line in corpusKeyWords.readlines():
+            splitedLine = line.split(',')
+            idRecipe = splitedLine[0]
+            recipeKeywords = splitedLine[1:len(splitedLine)-1]
+            last = re.sub('\n', '', splitedLine[len(splitedLine)-1])
+            recipeKeywords.append(last)
+            decision, prop = self.compare(list(queryKeywords), recipeKeywords)
+            if prop != 0 :
+                idRecipes[idRecipe] = prop
+                recipesTocheck[idRecipe] = (recipeKeywords, prop)
+        sortedIdRecipes = sorted(idRecipes.items(), key=itemgetter(1), reverse=True)
+
+        deniedList = self.getNonDesirable(checked)
+        checkedRecipes = {}
+        for recipeKey in recipesTocheck.keys():
+            recipe = recipesTocheck.get(recipeKey)
+            exist, i = False, 0
+            while i < len(deniedList) and not exist:
+                if  deniedList[i] in recipe[0]:
+                    exist = True
+                i += 1
+            if not exist:
+                checkedRecipes[recipeKey] =  recipe[1]
+
+        sortedCheckedRecipes = sorted(checkedRecipes.items(), key=lambda x: x[1], reverse=True)
+
+        return sortedIdRecipes, sortedCheckedRecipes
+
+
 
 
 
 
 if __name__ == "__main__":
-    search = Search(['abricots', 'soUpe', 'balsamique', 'feuilles', 'concombre', 'vinaigre','vinaigre','vinaigre','vinaigre',  'أكواب', 'chèvre', 'الجيلي'], 'French_corpus.txt')
-    text = search.getRecipe(1)
-    recipeKeyWords = search.getKeyWords(text)
-    print(recipeKeyWords)
+    search = Search(['france'], 'corpus/frCorpus.txt')
+    print(search.idSearchData(12, 1))
+    # text = search.getRecipe(1)
+    # recipeKeyWords = search.getKeyWords(text)
+    # print(recipeKeyWords)
     # recipe = search.idSearch(52)
     # print(recipe)
 
@@ -156,114 +298,3 @@ if __name__ == "__main__":
     # print(search.idSearch(52))
 
 
-
-
-
-#Recherche Minceur
-#nombre de calories 
-
-#KCAL='300'
-#def recherche_nutritioniste(soup,KCAL):
-#     for div in soup.find_all("rec"):
-#            for d in div.find_all("title"):
-#                if (d in HallalFood(soup)):
-#                    for(d1 in d.find_all("eng")):
-#                        print(d1.text)
-#recherche_nutritioniste(soup,KCAL) 
-
-
-
-
-#online 
-
-
-
-
-########################################""#recette de la semaine####################################################
-#online : 1st step
-
-
-# import locale
-# import datetime
-
-# locale.setlocale(locale.LC_ALL, locale="French")
-# now = datetime.datetime.now()
-
-# jour=now.strftime("%A")
-# url="http://www.cuisineaz.com/idees-repas/?jour="+jour
-
-# page=urlopen(url).read().decode('utf-8')
-    
-# soup1 = BeautifulSoup(page,'html.parser')
-
-# for div in soup1.find_all("article", attrs={'id':'ContentPlaceHolder_rptMenuElementRecette_elemListItem_0','class':'tile entree'}):
-#         dj=re.sub('[\n]+','',div.text)        
-#         print(dj)
-#         recherche_recette(soup,dj)
-# for div in soup1.find_all("article", attrs={'id':'ContentPlaceHolder_rptMenuElementRecette_elemListItem_1','class':'tile plat'}):
-#         dj=re.sub('[\n]+','',div.text)        
-#         print(dj)
-#         recherche_recette(soup,dj)
-# for div in soup1.find_all("article", attrs={'id':'ContentPlaceHolder_rptMenuElementRecette_elemListItem_2','class':'tile dessert'}):
-#         dj=re.sub('[\n]+','',div.text)        
-#         print(dj)
-#         recherche_recette(soup,dj)
-## Maj recette
-
-
-
-
-#Rapporter les noms des plats du jour 
-
-
-
-#offline : 2nd step
-#recherche_recette(soup,recette)   
-
-
-
-
-#######################################contrainte sur les ingredients##################################################### 
-
-
-
-
-
-
-
-
-
-##########################################Recherche video################################################################
-# supposons qu'on a un lien extrait avec chaque recette
-# on ouvre le lien on aura a le telechager 
-#TESTvIDEOS
-#lecteur video sur l'interface.
-
-
-
-########################################################################################################################
-
-# <rec>
-# <title>AbraCadaBra Gâteau Magique version salé ..</title>
-# <inf>Auteur : 
-# Niveau : Facile
-# Préparation : 15 min
-# Cuisson :35 min</inf>
-# <cat>Légumes</cat>
-# <cat>Poivron</cat>
-# <ing>2 œufs</ing>
-# <ing>25 cl de lait             </ing>
-# <ing>65 g de beurre </ing>
-# <ing>30 g d'emmental râpé</ing>
-# <ing>50 gr de Parmesan</ing>
-# <ing>70 gr de Grogonzola</ing>
-# <ing>100 g de lardons</ing>
-# <ing>½ poivron long en petits dés</ing>
-# <ing>55 gr de farine </ing>
-# <ing>¼ cc noix de muscade râpée </ing>
-# <ing>Sel, poivre du moulin </ing>
-# <prep>1°- Faites fondre le beurre, puis laissez-le tiédir. Dans une casserole, faites chauffer le lait et réservez.     2°- Séparez les blancs des jaunes d'oeufs dans deux saladiers. Blanchissez les jaunes avec le beurre fondu. 3°- Incorporez la farine en deux fois et versez le lait en deux ou trois fois aussi . 4°- Ajoutez l'emmental râpé et le parmesan , la noix de muscade, salez, poivrez et mélangez. 5°- Montez les blancs en neige** très ferme  puis, à l'aide d'une spatule, incorporez-les à la préparation précédente **  Pour incorporer les blancs en neige à la préparation très liquide, il est conseillé d’utiliser un fouet en travaillant la masse doucement,afin de ne pas la dissoudre dans la pâte : il doit rester de gros grumeaux. Il est en effet difficile d’incorporer des blancs à une préparation liquide avec une cuillère en bois.6°- Répartissez les lardons et le poivron coupez en petits dés au fond du moule, puis versez le mélange aux oeufs par-dessus. 07°- Mettre au four chaud 160 ° pour 35 mn. Le dessus du gâteau doit être doré et l'intérieur légèrement tremblotant.</prep>
-# <eng></eng>
-# </rec>
-#HallalFood
-# NHI='bacon,gélatine,jambon,porc,liqueur,biere,vin'
