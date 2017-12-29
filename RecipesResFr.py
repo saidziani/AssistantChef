50f6c8ce5850de9eaad6da1ebfa85d99e78272f3
Said <saidziani.pro@gmail.com>
2017-12-29 20:22:12 +0100

    temp changement.

--- RecipesResFr.py
+++ RecipesResFr.py
@@ -3,11 +3,14 @@ import search, OneRecipeFr
 import functools
 
 recipes = {}
-nbClick = 0
-nonEmpty = 0
+actualRecipeNb = 0
+actualNoneEmptyRecipeNb = 0
+precedentFive = []
 class RecipesResFr(object):
 
-    def setupUi(self, Form, data, checked, queryWords):
+    def setupUi(self, Form):
+
+        ['12846', '5323', '5', '5613', '11614', '3129', '10151', '5874', '12081', '16022', '5048', '3993', '4828', '13840']
 
         Form.setObjectName("Form")
         Form.setFixedSize(800, 500)
@@ -18,15 +21,14 @@ class RecipesResFr(object):
         Form.setStyleSheet("background-color: #f7f7f7;\n"
 "color: #424242;")
         
+        data = ['12846', '5323', '5', '5613', '11614', '3129', '10151', '5874', '12081', '16022', '5048', '3993', '4828', '13840', '9590', '5590', '1248', '3074', '15699', '4135', '15775', '16274', '3555', '7957', '10877', '16272', '2235', '7265', '13496', '15791', '13363', '13829', '6521', '15765', '15701', '1302', '12085', '668', '6153', '16478', '682', '14229', '1219', '15704', '8922', '2', '5977', '13511', '16031', '1642', '16024', '2453', '5553', '770', '11065', '6418', '13603', '9466', '3384', '16025', '8675', '5592', '16271', '4361', '8018', '13520', '3069', '3208', '1291', '4853', '5146', '15559', '3', '11229', '4464', '3010', '15838', '10661', '12386', '6024', '553', '1518', '16275', '8344', '6557', '3340', '4268', '14108', '1236', '3499', '2805', '15751', '15714', '13415', '2413', '9471', '2824', '11270', '5920', '15766', '10076', '6256', '1252', '5180', '16023', '4659', '16082', '16270', '2889', '2786', '12054', '4581', '6574', '8600', '15767', '16273', '2790', '1627', '13515', '3070', '4', '3009', '3754']
+
+        checked = []
+
+        queryWords = ['citron']
+
         Form.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.closeWindow = Form.close
-
-
-        self.labelNext = QtWidgets.QLabel(Form)
-        self.labelNext.setGeometry(QtCore.QRect(150, 430, 500, 30))
-        self.labelNext.setObjectName("labelRechNoline")
-        self.labelNext.setStyleSheet("font-size:16px;color:#bdbdbd")
-        self.labelNext.setText('Suivant >>')
 
 
         self.label = QtWidgets.QLabel(Form)
@@ -52,33 +54,32 @@ class RecipesResFr(object):
 
         sizeRecipes = len(data)
         print("I'm from RecipesFr:", data)
-        newData = []
+        noneEmptyRecipes = []
         for rec in data:
             if rec:
-                newData.append(rec)
+                noneEmptyRecipes.append(rec)
 
-        self.newData = newData
+        self.noneEmptyRecipes = noneEmptyRecipes
         self.label_3.setText(str(sizeRecipes)+' résultats de recherche')
 
-        global nonEmpty
+        global actualNoneEmptyRecipeNb
         h = 110
         i = 0
-        for info in newData:
+        for info in noneEmptyRecipes:
             idRecipe = info
             title, cat, ing, prep, eng, inf  = self.getRecipeTitle(idRecipe, checked)
             title = ', '.join(title)
-            nonEmpty += 1
+            actualNoneEmptyRecipeNb += 1
             if title:
                 tmpRecipe = [title, cat, ing, prep, eng, inf]
                 recipes[idRecipe] = tmpRecipe
-                
+                precedentFive.append(idRecipe)
                 if i == 0:
                     self.label_4 = QtWidgets.QLabel(Form)
-                    self.label_4.setGeometry(QtCore.QRect(150, h, 500, 25))
+                    self.label_4.setGeometry(QtCore.QRect(150, h, 600, 25))
                     self.label_4.setObjectName(idRecipe)
                     self.label_4.setText(title)
                     self.label_4.setStyleSheet("font-size:19px;color:#424242;font-weight:500")
-                    self.label_4.mousePressEvent = self.sendData
                     self.label_4.mousePressEvent = functools.partial(self.sendData, source_object=self.label_4)
 
                     cat = ' | '.join(cat)
@@ -91,11 +92,10 @@ class RecipesResFr(object):
 
                 if i == 1:
                     self.label_5 = QtWidgets.QLabel(Form)
-                    self.label_5.setGeometry(QtCore.QRect(150, h, 500, 25))
+                    self.label_5.setGeometry(QtCore.QRect(150, h, 600, 25))
                     self.label_5.setObjectName(idRecipe)
                     self.label_5.setText(title)
                     self.label_5.setStyleSheet("font-size:19px;color:#424242;font-weight:500")
-                    self.label_5.mousePressEvent = self.sendData
                     self.label_5.mousePressEvent = functools.partial(self.sendData, source_object=self.label_5)
 
                     cat = ' | '.join(cat)
@@ -108,11 +108,10 @@ class RecipesResFr(object):
 
                 if i == 2:
                     self.label_6 = QtWidgets.QLabel(Form)
-                    self.label_6.setGeometry(QtCore.QRect(150, h, 500, 25))
+                    self.label_6.setGeometry(QtCore.QRect(150, h, 600, 25))
                     self.label_6.setObjectName(idRecipe)
                     self.label_6.setText(title)
                     self.label_6.setStyleSheet("font-size:19px;color:#424242;font-weight:500")
-                    self.label_6.mousePressEvent = self.sendData
                     self.label_6.mousePressEvent = functools.partial(self.sendData, source_object=self.label_6)
 
                     cat = ' | '.join(cat)
@@ -125,11 +124,10 @@ class RecipesResFr(object):
 
                 if i == 3:
                     self.label_7 = QtWidgets.QLabel(Form)
-                    self.label_7.setGeometry(QtCore.QRect(150, h, 500, 25))
+                    self.label_7.setGeometry(QtCore.QRect(150, h, 600, 25))
                     self.label_7.setObjectName(idRecipe)
                     self.label_7.setText(title)
                     self.label_7.setStyleSheet("font-size:19px;color:#424242;font-weight:500")
-                    self.label_7.mousePressEvent = self.sendData
                     self.label_7.mousePressEvent = functools.partial(self.sendData, source_object=self.label_7)
 
                     cat = ' | '.join(cat)
@@ -142,11 +140,10 @@ class RecipesResFr(object):
 
                 if i == 4:
                     self.label_8 = QtWidgets.QLabel(Form)
-                    self.label_8.setGeometry(QtCore.QRect(150, h, 500, 25))
+                    self.label_8.setGeometry(QtCore.QRect(150, h, 600, 25))
                     self.label_8.setObjectName(idRecipe)
                     self.label_8.setText(title)
                     self.label_8.setStyleSheet("font-size:19px;color:#424242;font-weight:500")
-                    self.label_8.mousePressEvent = self.sendData
                     self.label_8.mousePressEvent = functools.partial(self.sendData, source_object=self.label_8)
 
                     cat = ' | '.join(cat)
@@ -170,7 +167,20 @@ class RecipesResFr(object):
         self.labelRechNoline.setText("Recherche infructueuse ? faites une recherhce en ligne.")
         self.labelRechNoline.mousePressEvent = functools.partial(self.doOnlineSearche)
 
-        self.labelNext.mousePressEvent = self.display
+        if len(noneEmptyRecipes) > 5:
+            self.labelNext = QtWidgets.QLabel(Form)
+            self.labelNext.setGeometry(QtCore.QRect(260, 430, 100, 30))
+            self.labelNext.setObjectName("labelNext")
+            self.labelNext.setStyleSheet("font-size:16px;color:#424242;opacity:0.6")
+            self.labelNext.setText('Suivant >>')
+            self.labelNext.mousePressEvent = functools.partial(self.nextORprecDisplay, source_object=self.labelNext)
+
+        self.labelPreced = QtWidgets.QLabel(Form)
+        self.labelPreced.setGeometry(QtCore.QRect(150, 430, 110, 30))
+        self.labelPreced.setObjectName("labelPreced")
+        self.labelPreced.setStyleSheet("font-size:16px;color:#424242;opacity:0.6")
+        self.labelPreced.setText('<< Précédent |')
+        self.labelPreced.mousePressEvent = functools.partial(self.nextORprecDisplay, source_object=self.labelPreced)
 
 
         self.retranslateUi(Form)
@@ -212,47 +222,80 @@ class RecipesResFr(object):
         self.window.show()
 
 
-    def display(self, position):
-        global nbClick
-        global nonEmpty
-        nonEmptyNb = nonEmpty
+    def nextORprecDisplay(self, event, source_object=None):
+
+        global actualNoneEmptyRecipeNb
+        global precedentFive
+
         i = 0
-        nbClick += 5
-        for info in self.newData[nonEmptyNb:]:
+        startPoint = actualNoneEmptyRecipeNb
+
+        myList = self.noneEmptyRecipes[startPoint:]
+        if source_object.objectName()=="labelPreced":
+            myList = precedentFive
+
+        for info in myList:
             idRecipe = info
             title, cat, ing, prep, eng, inf  = self.getRecipeTitle(idRecipe, [])
             title = ', '.join(title)
-            nonEmpty += 1
+            if source_object.objectName()=="labelNext":
+                actualNoneEmptyRecipeNb += 1
             if title:
+                tmpRecipe = [title, cat, ing, prep, eng, inf]
+                recipes[idRecipe] = tmpRecipe
+
                 if i == 0:
-                    self.label_4.setStyleSheet("color:#424242;")
+                    self.label_4.setStyleSheet("font-size:19px;color:#424242;font-weight:500")
                     self.label_4.setText(title)
+                    self.label_4.setObjectName(idRecipe)
                     cat = ' | '.join(cat)
                     self.labelL_4.setText(cat)
+                    self.label_4.mousePressEvent = functools.partial(self.sendData, source_object=self.label_4)
+                    if source_object.objectName()=="labelNext":
+                        precedentFive[0] = idRecipe
+
                 if i == 1:
-                    self.label_5.setStyleSheet("color:#424242;")
+                    self.label_5.setStyleSheet("font-size:19px;color:#424242;font-weight:500")
                     self.label_5.setText(title)
+                    self.label_5.setObjectName(idRecipe)
                     cat = ' | '.join(cat)
                     self.labelL_5.setText(cat)
+                    self.label_5.mousePressEvent = functools.partial(self.sendData, source_object=self.label_5)
+                    if source_object.objectName()=="labelNext":
+                        precedentFive[1] = idRecipe
+
                 if i == 2:
-                    self.label_6.setStyleSheet("color:#424242;")
+                    self.label_6.setStyleSheet("font-size:19px;color:#424242;font-weight:500")
                     self.label_6.setText(title)
+                    self.label_6.setObjectName(idRecipe)
                     cat = ' | '.join(cat)
                     self.labelL_6.setText(cat)
+                    self.label_6.mousePressEvent = functools.partial(self.sendData, source_object=self.label_6)
+                    if source_object.objectName()=="labelNext":
+                        precedentFive[2] = idRecipe
+
                 if i == 3:
-                    self.label_7.setStyleSheet("color:#424242;")
+                    self.label_7.setStyleSheet("font-size:19px;color:#424242;font-weight:500")
                     self.label_7.setText(title)
+                    self.label_7.setObjectName(idRecipe)
                     cat = ' | '.join(cat)
                     self.labelL_7.setText(cat)
+                    self.label_7.mousePressEvent = functools.partial(self.sendData, source_object=self.label_7)
+                    if source_object.objectName()=="labelNext":
+                        precedentFive[3] = idRecipe
+
                 if i == 4:
-                    self.label_8.setStyleSheet("color:#424242;")
+                    self.label_8.setStyleSheet("font-size:19px;color:#424242;font-weight:500")
                     self.label_8.setText(title)
+                    self.label_8.setObjectName(idRecipe)
                     cat = ' | '.join(cat)
                     self.labelL_8.setText(cat)
+                    self.label_8.mousePressEvent = functools.partial(self.sendData, source_object=self.label_8)
+                    if source_object.objectName()=="labelNext":
+                        precedentFive[4] = idRecipe
                 i += 1
             if i == 5:
                 break
-        
 
 if __name__ == "__main__":
     import sys
